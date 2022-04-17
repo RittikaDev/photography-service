d70a81bede6c850fc95887a4b15dce1da966a7f3
RittikaDev <rittika.dev@gmail.com>
2022-04-18 00:15:33 +0600

    environment variable added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Haeder.css";
 import { Container, Form, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../../firebase.init";
 import { signOut } from "firebase/auth";
+import CustomLink from "../../../CustomLink/CustomLink";
 
 const Header = () => {
   const [user] = useAuthState(auth);
@@ -21,43 +21,43 @@ const Header = () => {
         <Navbar.Collapse id="navbarScroll navbar">
           <Nav className="ms-auto my-1 my-lg-0" navbarScroll>
             <Form className="d-flex collapse">
-              <Link to="/banner" className="nav-link">
+              <CustomLink to="/banner" className="nav-link">
                 Home
-              </Link>
-              <Link className="nav-link" to="/recent">
+              </CustomLink>
+              <CustomLink className="nav-link" to="/recent">
                 Category
-              </Link>
-              <Link className="nav-link" to="/about">
+              </CustomLink>
+              <CustomLink className="nav-link" to="/about">
                 About
-              </Link>
+              </CustomLink>
 
               {user ? (
-                <Link className="nav-link" to="" onClick={handleSignout}>
+                <CustomLink className="nav-link" to="" onClick={handleSignout}>
                   Signout
-                </Link>
+                </CustomLink>
               ) : (
-                <Link className="nav-link" to="/login">
+                <CustomLink className="nav-link" to="/login">
                   Signup
-                </Link>
+                </CustomLink>
               )}
             </Form>
           </Nav>
           {/* <Form className="d-flex collapse">
-            <Link className="nav-link search-box" to="">
+            <CustomLink className="nav-link search-box" to="">
               <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
-            </Link>
-            <Link className="nav-link facebook" to="">
+            </CustomLink>
+            <CustomLink className="nav-link facebook" to="">
               <FontAwesomeIcon icon={faFacebookF}></FontAwesomeIcon>
-            </Link>
-            <Link className="nav-link twitter" to="">
+            </CustomLink>
+            <CustomLink className="nav-link twitter" to="">
               <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-            </Link>
-            <Link className="nav-link instagram" to="">
+            </CustomLink>
+            <CustomLink className="nav-link instagram" to="">
               <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
-            </Link>
-            <Link className="nav-link dribble" to="">
+            </CustomLink>
+            <CustomLink className="nav-link dribble" to="">
               <FontAwesomeIcon icon={faDribbble}></FontAwesomeIcon>
-            </Link>
+            </CustomLink>
           </Form> */}
         </Navbar.Collapse>
       </Container>
