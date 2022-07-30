64f03c8b3a539c88d466ac14c679f3a7719e402b
RittikaDev <rittika.dev@gmail.com>
2022-07-30 18:21:39 +0600

    updates made

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,38 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Contact.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <div className="wrapper my-3">
-      <h3>Please Login</h3>
-      <form className="container card-signup d-block form">
-        <input
-          name="name"
-          type="text"
-          className="feedback-input"
-          placeholder="Name"
-        />
-        <input
-          name="email"
-          type="text"
-          className="feedback-input"
-          placeholder="Email"
-        />
-        <p
-          className="text-danger"
-          style={{ fontSize: "12px", fontWeight: "bolder" }}
-        ></p>
+	const sendEmail = (e) => {
+		e.preventDefault();
+		console.log(e.target);
 
-        <input
-          name="password"
-          type="password"
-          className="feedback-input"
-          placeholder="Password"
-        />
-
-        <input type="submit" value="Register" className="button" />
-      </form>
-    </div>
-  );
+		// emailjs
+		// 	.sendForm(
+		// 		"service_4zgcava",
+		// 		"template_fpu5rnt",
+		// 		e.target,
+		// 		"bxY2HNrT9lXszTIDK"
+		// 	)
+		// 	.then((res) => {
+		// 		console.log(res);
+		// 	})
+		// 	.catch((err) => console.log(err));
+	};
+	return (
+		<div className="container contactSection">
+			<div className="contactSection__wrapper">
+				<div className="left my-5">
+					<div className="itemstyle">
+						<div className="icon">icon</div>
+						<div className="info">
+							<p>text</p>
+						</div>
+					</div>
+					<div className="itemstyle">
+						<div className="icon">icon</div>
+						<div className="info">
+							<p>text</p>
+						</div>
+					</div>
+					<div className="itemstyle">
+						<div className="icon">icon</div>
+						<div className="info">
+							<p>text</p>
+						</div>
+					</div>
+					{/* <ContactInfoItem icon={<MdLocalPhone />} text="+8801231" />
+					<ContactInfoItem icon={<MdEmail />} text="webcifar@gmail.com" />
+					<ContactInfoItem text="Chittagong, Bangladesh" /> */}
+				</div>
+				<div className="right">
+					<form onSubmit={sendEmail} className="wrapper-contact my-3">
+						<div className="form-group">
+							<label htmlFor="name">
+								Your Name
+								<input type="text" id="name" name="name" />
+							</label>
+						</div>
+						<div className="form-group">
+							<label htmlFor="email">
+								Your Email
+								<input type="email" id="email" name="email" />
+							</label>
+						</div>
+						<div className="form-group">
+							<label htmlFor="message">
+								Your message
+								<textarea type="text" id="message" name="message" />
+							</label>
+						</div>
+						<button type="submit">Send</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,12 +7,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+	// apiKey: process.env.REACT_APP_apiKey,
+	// authDomain: process.env.REACT_APP_authDomain,
+	// projectId: process.env.REACT_APP_projectId,
+	// storageBucket: process.env.REACT_APP_storageBucket,
+	// messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	// appId: process.env.REACT_APP_appId,
+	apiKey: "AIzaSyA0iucHiK-iyu8usWtcjHIBjF4tyz-hlRM",
+	authDomain: "photography-service-3b352.firebaseapp.com",
+	projectId: "photography-service-3b352",
+	storageBucket: "photography-service-3b352.appspot.com",
+	messagingSenderId: "335483145801",
+	appId: "1:335483145801:web:ea4a3a2df78e35f53df998",
 };
 
 // Initialize Firebase
