9aa2fc529014e1c12bc0df5a8499986cb3c71952
RittikaDev <rittika.dev@gmail.com>
2022-08-03 18:28:22 +0600

    cart

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Services.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Services = () => {
 	const [services, setServices] = useState([]);
+	const [changeIconID, setChangeIconID] = useState([]);
+	const [selectedArray, setSelectedArray] = useState([]);
 	useEffect(() => {
 		fetch("data.json")
 			.then((res) => res.json())
@@ -11,7 +15,16 @@ const Services = () => {
 				setServices(data);
 			});
 	}, []);
-	console.log(services);
+
+	const addToCart = (id) => {
+		const newIcons = [...changeIconID, id];
+		setChangeIconID(newIcons);
+		let selected = services.find((service) => service.id === id);
+		selected.icon = "true";
+		let selectedArr = [...selectedArray, selected];
+		setSelectedArray(selectedArr);
+		console.log(selectedArray);
+	};
 	return (
 		<div className="container body">
 			{services.map((service) => (
@@ -25,6 +38,17 @@ const Services = () => {
 								{service.name}
 								<br />
 								<span>{service.price}</span>
+								<span
+									onClick={() => {
+										addToCart(service.id);
+									}}
+								>
+									{changeIconID.includes(service.id) ? (
+										<FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
+									) : (
+										<FontAwesomeIcon icon={faCartPlus}></FontAwesomeIcon>
+									)}
+								</span>
 							</h3>
 						</div>
 					</div>
